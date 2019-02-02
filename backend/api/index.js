5bc991e66ddfe7de2768bb9b3b9bcd12674004eb
NimaQu <admin@nimaqu.com>
2019-02-02 22:21:42 +0800

    【U】修复token效验失败的BUG

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,12 +1,14 @@
 module.exports = () => {
   return (req, res, next) => {
-    let whiteList = ['/api/findarticletype', '/api/onefindarticle', '/api/findarticle', '/api/loginuser', '/api/newuser']
+    let whiteList = ['', '/api/findarticletype', '/api/onefindarticle', '/api/findarticle', '/api/loginuser', '/api/newuser', '/api/getimg']
     let url = req.originalUrl
     if (url.indexOf('?')) {
       url = url.substring(0, url.indexOf('?'))
     }
-    if (whiteList.indexOf(url) !== -1) {
+    if (whiteList.indexOf(url) === -1) {
       if (!req.headers.ticani9) {
+        console.log(req)
+        console.log(whiteList.indexOf(url))
         res.send({ code: 400, data: '身份验证失效，请重新登入' })
         return
       }
